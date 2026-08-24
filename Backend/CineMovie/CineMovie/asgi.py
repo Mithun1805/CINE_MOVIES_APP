1ef3c763605309e5833529be0f5f3373796d37d4
Mithun1805 <mithun18005@gmail.com>
2026-08-24 23:07:04 +0530

    backend folder alone successfully created with django Framework

---
+++ Backend/CineMovie/CineMovie/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CineMovie project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CineMovie.settings')
+
+application = get_asgi_application()
